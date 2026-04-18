ac7dc5e515ce9bc12f89a05222ed596edb9512ef
BillOrf7 <hassan.belorf@student.odisee.be>
2026-04-18 14:13:18 +0200

    Models en formule aanmaken

--- aircalc_app/models.py
+++ aircalc_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DragCalculation(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    air_density = models.FloatField()        # ρ (kg/m³)
+    velocity = models.FloatField()           # v (m/s)
+    frontal_area = models.FloatField()       # A (m²)
+    drag_coefficient = models.FloatField()   # C_w
+    drag_force = models.FloatField()         # F_D (N)
+    power = models.FloatField()              # P (W)
+
+    def __str__(self):
+        return f"{self.date.strftime('%Y-%m-%d %H:%M')} - F_D={self.drag_force:.2f} N"
+
